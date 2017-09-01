40fe744fa6328ec3dd68dfe2873a24c2fa607d89
Zahraa Horeibi <zahraahoreibi@hotmail.com>
2017-09-01 19:32:15 -0400

    Completed part 2

--- app/src/main/java/ca/campbell/simplecalc/MainActivity.java
+++ app/src/main/java/ca/campbell/simplecalc/MainActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.regex.*;
+
 //  TODO: add buttons & methods for subtract, multiply, divide
 //  TODO: extra input validation: no divide by zero
 
@@ -16,6 +18,9 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     EditText etNum1, etNum2;
     TextView result;
+    private TextView msgHeaderNum1, msgHeaderNum2, msg;
+    String num1Str, num2Str;
+
     double num1, num2;
 
     @Override
@@ -26,13 +31,86 @@ public class MainActivity extends AppCompatActivity {
         etNum1 = (EditText) findViewById(R.id.num1);
         etNum2 = (EditText) findViewById(R.id.num2);
         result = (TextView) findViewById(R.id.result);
+
     }  //onCreate()
 
     // TODO: input validation: set text to show error
     public void addNums(View v) {
-        num1 = Double.parseDouble(etNum1.getText().toString());
-        num2 = Double.parseDouble(etNum2.getText().toString());
-        result.setText(Double.toString(num1 + num2));
+        num1Str = etNum1.getText().toString();
+        num2Str = etNum2.getText().toString();
+
+        if (isValid(num1Str) && isValid(num2Str)) {
+            num1 = Double.parseDouble(num1Str);
+            num2 = Double.parseDouble(num2Str);
+            result.setText(Double.toString(num1 + num2));
+        }
+        else
+            result.setText(R.string.invInput);
+
+
+
     }  //addNums()
+
+    public void substractNums(View v){
+        num1Str = etNum1.getText().toString();
+        num2Str = etNum2.getText().toString();
+
+        if (isValid(num1Str) && isValid(num2Str)) {
+            num1 = Double.parseDouble(num1Str);
+            num2 = Double.parseDouble(num2Str);
+            result.setText(Double.toString(num1 - num2));
+        }
+        else
+            result.setText(R.string.invInput);
+    }
+
+    public void divideNums(View v){
+        num1Str = etNum1.getText().toString();
+        num2Str = etNum2.getText().toString();
+
+        if ( isValid(num1Str) && num2Str.equals("0")) {
+            // display error message
+            result.setText(R.string.invDivision);
+        }
+
+        else if (isValid(num1Str) && isValid(num2Str)) {
+            num1 = Double.parseDouble(num1Str);
+            num2 = Double.parseDouble(num2Str);
+            result.setText(Double.toString(num1 / num2));
+        }
+
+        else
+            result.setText(R.string.invInput);
+
+    }
+
+    public void multiplyNums(View v) {
+        num1Str = etNum1.getText().toString();
+        num2Str = etNum2.getText().toString();
+
+        if (isValid(num1Str) && isValid(num2Str)) {
+            num1 = Double.parseDouble(num1Str);
+            num2 = Double.parseDouble(num2Str);
+            result.setText(Double.toString(num1 * num2));
+        }
+        else
+            result.setText(R.string.invInput);
+    }
+
+    public boolean isValid(String num){
+        String regexStr = "-?[0-9]*[.]?[0-9]+";
+
+        if(num.matches(regexStr))
+            return true;
+
+        else
+            return false;
+    }
+
+    public void clearData(View v){
+        etNum1.setText("");
+        etNum2.setText("");
+        result.setText("");
+    }
 
 }
